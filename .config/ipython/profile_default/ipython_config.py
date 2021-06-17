d63b48900fda3ccd73982ed218b2487cfd367530
Piotr Machura <piotr.machura16@gmail.com>
2021-06-17 19:21:55 +0200

    Remove github cli

--- .config/ipython/profile_default/ipython_config.py
+++ .config/ipython/profile_default/ipython_config.py
@@ -18,3 +18,10 @@ c.TerminalInteractiveShell.prompt_includes_vi_mode = False
 c.TerminalInteractiveShell.term_title = True
 c.TerminalInteractiveShell.term_title_format = 'IPython ' + sys.version.split(' ')[0]
 c.TerminalInteractiveShell.true_color = False
+c.InteractiveShellApp.exec_lines = [
+    'from matplotlib.pylab import *',
+    'from matplotlib import pyplot as plt',
+    'import numpy as np',
+    'from IPython.display import display',
+    'from IPython.core.pylabtools import figsize, getfigs',
+]
